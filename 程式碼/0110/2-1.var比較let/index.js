8058e7b6f39402d71fadd7f8a27e25bf75f69637
Eddy Chang <eddy@joomla.com.tw>
2024-01-10 14:21:25 +0800

    Update index.js

--- 程式碼/0110/2-1.var比較let/index.js
+++ 程式碼/0110/2-1.var比較let/index.js
@@ -1,11 +1,15 @@
 // 使用var的"函式作用域"
+// 因為var沒有區塊作用域，所以for迴圈結束後，i仍然存在
 for (var i = 0; i < 10; i++) {
   console.log('i=', i)
 }
 
 console.log('after for loop, i=', i)
 
-// 比較使用let的"區塊作用域"
+//----------------------------------------------
+
+// 使用let的"區塊作用域"
+// 因為let有區塊作用域，所以for迴圈結束後，j不存在
 for (let j = 0; j < 10; j++) {
   console.log('j=', j)
 }
