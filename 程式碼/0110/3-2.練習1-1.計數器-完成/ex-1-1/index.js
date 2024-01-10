f04afe745d52eaecf748d9f091880b13be082b72
Eddy Chang <eddy@joomla.com.tw>
2024-01-10 16:27:49 +0800

    Update index.js

--- 程式碼/0110/3-2.練習1-1.計數器-完成/ex-1-1/index.js
+++ 程式碼/0110/3-2.練習1-1.計數器-完成/ex-1-1/index.js
@@ -1,9 +1,14 @@
+// total是由id="total"的元素物件
 const total = document.getElementById('total')
 
+// total.innerText是total元素物件的文字內容
+// 也可以用來設定文字內容
 // console.log(total.innerText)
-
 // total.innerText = 888
 
+// total.addEventListener是total元素物件的事件監聽器
+// 第一個參數是事件名稱，第二個參數是事件發生時要執行的函式
 total.addEventListener('click', function () {
+  // 從網頁上得到的文字是字串類型，Number()是將文字轉成數字
   total.innerText = Number(total.innerText) + 1
 })
