3964c946fc5a6891bf389ac46b46400bf4a6f7ee
Eddy Chang <eddy@joomla.com.tw>
2024-01-11 14:28:43 +0800

    Update index.js

--- 程式碼/0111/4-1.布林falsy與truthy-預設指定值語法/index.js
+++ 程式碼/0111/4-1.布林falsy與truthy-預設指定值語法/index.js
@@ -1,4 +1,8 @@
 function add(x, y) {
+  // falsy: false, 0, '', null, undefined, NaN
+  // 這裡的 || 是或的意思，用的是falsy判斷
+  // 如果 x 是 falsy，則 x1 = 1，否則 x1 = x
+  // 指定預設值
   const x1 = x || 1
   const y1 = y || 2
   console.log(x, y)
