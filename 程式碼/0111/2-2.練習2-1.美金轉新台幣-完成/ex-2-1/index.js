5f44b2044216e156e1b002d668cb7ae79d4690f7
Eddy Chang <eddy@joomla.com.tw>
2024-01-11 11:05:44 +0800

    Update index.js

--- 程式碼/0111/2-2.練習2-1.美金轉新台幣-完成/ex-2-1/index.js
+++ 程式碼/0111/2-2.練習2-1.美金轉新台幣-完成/ex-2-1/index.js
@@ -1,5 +1,7 @@
 // 1. 獲取網頁上的DOM元素
 // input
+// 以下定義為jsdoc定義，目的只是為了告訴vscode編輯器，此元素為
+// HTMLInputElement，而不是一般的HTMLElement，可提供更多自動完成協助
 /**
  * @type HTMLInputElement
  */
@@ -18,7 +20,7 @@ usd2twd.addEventListener('click', function () {
   const v = Number(money.value)
 
   // 2. 乘以 31.10 呈現在result.innerText
-  // 使用 toFixed方法(數字用->轉字串)，可以格式化小數點後保留幾位
+  // 使用 toFixed方法(數字用->轉字串)，可以格式化小數點後保留幾位，會作四捨五入
   result.innerText = '新台幣:' + (v * 31.1).toFixed(0)
 })
 
@@ -26,7 +28,7 @@ twd2usd.addEventListener('click', function () {
   // 1. 得到money輸入的數字(記得文字要轉成數字)
   const v = Number(money.value)
 
-  // 2. 乘以 31.10 呈現在result.innerText
-  // 使用 toFixed方法(數字用->轉字串)，可以格式化小數點後保留幾位
+  // 2. 除以 31.10 呈現在result.innerText
+  // 使用 toFixed方法(數字用->轉字串)，可以格式化小數點後保留幾位，會作四捨五入
   result.innerText = '美金:' + (v / 31.1).toFixed(2)
 })
