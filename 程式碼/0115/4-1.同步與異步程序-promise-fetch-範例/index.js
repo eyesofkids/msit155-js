fc99a99661b298ca4a418baa3449a95edc60f914
Eddy Chang <eddy@joomla.com.tw>
2024-01-15 14:42:56 +0800

    Update index.js

--- 程式碼/0115/4-1.同步與異步程序-promise-fetch-範例/index.js
+++ 程式碼/0115/4-1.同步與異步程序-promise-fetch-範例/index.js
@@ -1,26 +1,33 @@
+// fetch 範例
+// 伺服器的網址，回應為 JSON 格式
 const serverUrl =
   'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users'
 
+// 顯示資料用的函式
 const log = (users) => {
   console.log(users)
-  document.getElementById('user_id1').innerHTML =
-    `name: ${users[0].name} age: ${users[0].age}`
+  document.getElementById(
+    'user_id1'
+  ).innerHTML = `name: ${users[0].name} age: ${users[0].age}`
 }
 
+// 使用 fetch 來取得資料
 fetch(serverUrl, { method: 'get' })
+  // 用 then 來接收回應
   .then(function (response) {
-    //處理
+    // 轉成 json，然後回傳另個 promise
     return response.json()
   })
+  // 用 then 來接收資料，解析後的資料會在 data 變數中
   .then(function (data) {
     log(data)
     return data
   })
+  // 用 catch 錯誤處理用
   .catch(function (err) {
     // Error :(
   })
 
-  
 // 同步程序
 // let a = 1
 // let b = a + 1 + 4 - 100
